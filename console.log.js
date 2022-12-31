9715d7b5ffbcb24c6a26a6140e3c26c3bd85c5b6
vuvietduc.com <117717197+vuvietduc-com@users.noreply.github.com>
2022-12-31 15:36:13 +0700

    Create console.log.js

---
+++ console.log.js
@@ -0,0 +1,28 @@
+let blackBackground=[
+    "font-size: 20px",
+    "color: #feb600",
+    "border-radius:5px",
+    "border: 2px solid #feb600",
+    "padding:5px 10px", ].join(" ;"); let link=[
+    "font-size: 18px",
+    "background-color: #FF99D7",
+    "padding:1px 10px",
+    "border-radius:5px"
+    ].join(" ;");
+    console.log(
+    "%cFrom vuvietduc.com with Love",
+    blackBackground,
+    );
+    console.log('%c👉 http://vuvietduc.com 👈',
+    link
+    );
+    console.log(' ')
+    const person={ name: '🖖Vũ việt Đức',
+    bird: '🤏2004',
+    Gender:'🙆‍♂️Male',
+    Education:'✍Hight School Only',
+    address:'🙏Vietnam',
+    Skill:'🙌HTM CSS Javascript Reactjs',
+    Skill:'🙌Design, Photograpy',
+    Blog:'👉VuVietDuc.com'};
+    console.table(person) 
